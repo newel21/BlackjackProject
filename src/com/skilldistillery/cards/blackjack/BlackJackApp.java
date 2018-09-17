888788450d6d3c7210e18859c11bb4f982c22a41
newel <newel_21@yahoo.com>
2018-09-16 20:11:20 -0600

    Updated: prompted user to HIT or STAY, then process according to user's choice

--- src/com/skilldistillery/cards/blackjack/BlackJackApp.java
+++ src/com/skilldistillery/cards/blackjack/BlackJackApp.java
@@ -2,7 +2,7 @@ package com.skilldistillery.cards.blackjack;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Scanner;
 
 import com.skilldistillery.cards.common.*;
 
@@ -10,6 +10,8 @@ public class BlackJackApp {
 
 	Deck deck = new Deck();	
 	private List<Card> initialCards;
+	private List<Card> playerCards = new ArrayList<>();
+	private List<Card> dealerCards = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		BlackJackApp play = new BlackJackApp();
@@ -17,6 +19,7 @@ public class BlackJackApp {
 		play.dealInitialCards();
 		play.giveTwoCardsToDealer();
 		play.giveTwoCardstoPlayer();
+		play.hitOrStay();
 		
 	}
 
@@ -28,10 +31,10 @@ public class BlackJackApp {
 	// 2. Deal initial 4 cards from the deck
 	private void dealInitialCards() {
 		initialCards = deck.getInitialCards();
-		Card dealerCard1 = initialCards.get(0);
-		Card dealerCard2 = initialCards.get(1);
-		Card playerCard1 = initialCards.get(2);
-		Card playerCard2 = initialCards.get(3);
+		dealerCards.add(initialCards.get(0));
+		dealerCards.add(initialCards.get(1));
+		playerCards.add(initialCards.get(2));
+		playerCards.add(initialCards.get(3));
 		
 	}
 	// 3. Give the first 2 cards to the dealer, then print ONLY the 
@@ -44,9 +47,55 @@ public class BlackJackApp {
 	// 4. Give the other 2 cards to the player, then print the value
 	//	of both cards to the console
 		public void giveTwoCardstoPlayer() {
-			System.out.println();
+			System.out.println("*********************************");
 			System.out.println("Player Card 1: " + initialCards.get(2));
 			System.out.println("Player Card 2: " + initialCards.get(3));					
+		}
+		
+	// 5. The user is prompted an option to HIT or STAY
+		public void hitOrStay() {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("*********************************");
+			System.out.println("Do you want to HIT or STAY? (H/S)");
+			switch (sc.nextLine()) {
+				case "H":
+				case "h":
+				case "Hit":
+				case "hit":
+					processHit();
+					hitOrStay();
+					break;
+				case "S":
+				case "s":
+				case "Stay":
+				case "stay":
+					break;
+			}
+			sc.close();		
+		}
+		
+	// 6. When the user selects HIT, a card will be dealt from the deck
+		public void processHit() {
+			Card newCard = deck.dealCard();
+			System.out.println("Player added: " + newCard);
+			playerCards.add(newCard);
+		}
+		
+	// 7. When the user selects STAY, calculate the final value of player's cards.
+	// if final value exceeds 21, player BUSTS! else, dealer has to open secret card.
+		int playerFinalValue = 0;
+		public void processStay() {
+			for (Card card : playerCards) {
+				playerFinalValue += card.getValue();
+			}
+			if (playerFinalValue > 21) {
+				System.out.println("Your final score is: " + playerFinalValue );
+				System.out.println("YOU BUST!!!" );
+			}
+			else {
+				System.out.println("Your final score is: " + playerFinalValue);
+				System.out.println("The dealer's secret card is " + initialCards.get(0));
+			}
 		}
 
 }
